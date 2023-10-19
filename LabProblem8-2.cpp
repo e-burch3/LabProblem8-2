787a8b826f687510a7d2e8bc0a96d5435197dd03
Ethan <e-burch@onu.edu>
2023-10-19 13:21:42 -0400

    completed countWords function

--- LabProblem8-2.cpp
+++ LabProblem8-2.cpp
@@ -10,8 +10,26 @@
 
 using namespace std;
 
-
+int countWords(string str);
 
 int main(void) {
+	string input;
+	cout << "Etner string: " << endl;
+	getline(cin, input);
+	cout << "Number of words: " << countWords(input);
 	return 0;
+}
+int countWords(string str) {
+	int count = 0;
+	char currChar;
+	char prevChar = ' ';
+	str += ' ';
+	for (int i = 0; i < str.length(); i++) {
+		currChar = str.at(i);
+		if (prevChar != ' ' && currChar == ' ') {
+			count++;
+		}
+		prevChar = currChar;
+	}
+	return count;
 }
