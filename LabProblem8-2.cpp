5842eb42bfa55e6c328d9b09ccd637a39920cf75
Ethan <e-burch@onu.edu>
2023-10-19 13:12:53 -0400

    Add project files.

---
+++ LabProblem8-2.cpp
@@ -0,0 +1,17 @@
+/*
+* File: LabProblem8-2.cpp
+*
+* Author: Ethan Burch
+* Description: counts the number of words separated by spaces in a string
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main(void) {
+	return 0;
+}
